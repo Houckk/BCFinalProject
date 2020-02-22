b153e0a58123fc9aa492693e7a03f9aa314210b7
Kenyan Houck <houckk@bc.edu>
2020-02-22 11:21:06 -0500

    Updates

---
+++ precache-manifest.b92b6b88a8cad18c23285706dcc769c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a101f7fe57178ad04219390a4e283625",
+    "url": "/social-media-houckk/index.html"
+  },
+  {
+    "revision": "f07b416c07fd7706eca5",
+    "url": "/social-media-houckk/static/css/main.4cc0633e.chunk.css"
+  },
+  {
+    "revision": "e8e3ca80316a5d03a0a8",
+    "url": "/social-media-houckk/static/js/2.ca0a147e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/social-media-houckk/static/js/2.ca0a147e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07b416c07fd7706eca5",
+    "url": "/social-media-houckk/static/js/main.c7e07fc6.chunk.js"
+  },
+  {
+    "revision": "797db0f5269dc4fa1657",
+    "url": "/social-media-houckk/static/js/runtime-main.a28ff03c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a33f31b6ac57216dfe4b71da916aa99.js"
+  "/social-media-houckk/precache-manifest.b92b6b88a8cad18c23285706dcc769c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-media-houckk/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
